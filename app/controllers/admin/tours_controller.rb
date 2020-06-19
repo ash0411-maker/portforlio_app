e54aa5085e19fb9c4221940706d93e54ba9ce1ad
ash <uetani.0411@gmail.com>
2020-06-19 21:54:27 +0900

    [add] Admin　ツアー一覧ページ実装

---
+++ app/controllers/admin/tours_controller.rb
@@ -0,0 +1,14 @@
+class Admin::ToursController < ApplicationController
+  def index
+    @genres = Genre.all
+    if params[:genre_id]
+      @genre = Genre.find(params[:genre_id])
+      @tours = @genre.tours.order(created_at: :desc)
+    else
+      @tours = Tour.all
+    end
+  end
+
+  def show
+  end
+end
