527face32bb1d8dd776b164392d1672fc883bb6f
ash <uetani.0411@gmail.com>
2020-06-19 20:24:29 +0900

    [add] Home ツアートップ画面の作成

--- app/controllers/home/tours_controller.rb
+++ app/controllers/home/tours_controller.rb
@@ -1,7 +1,12 @@
+# frozen_string_literal: true
+
 class Home::ToursController < ApplicationController
   def index
+    @tours = Tour.all.page(params[:page]).per(9)
   end
 
   def show
+    @tour = Tour.find(params[:id])
+    @tour_photos = @tour.tour_photos
   end
 end
