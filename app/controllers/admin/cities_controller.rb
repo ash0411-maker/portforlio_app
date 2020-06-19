199e56323a4b1a94636813ba8fa9591a1bf0df1f
ash <uetani.0411@gmail.com>
2020-06-19 14:37:31 +0900

    [add] Admin Genre/City投稿機能の実装

--- app/controllers/admin/cities_controller.rb
+++ app/controllers/admin/cities_controller.rb
@@ -1,2 +1,47 @@
 class Admin::CitiesController < ApplicationController
+  before_action :authenticate_admin!
+
+  def index
+    @city = City.new
+    @cities = City.all
+  end
+
+  def create
+    @city = City.new(city_params)
+    if @city.save
+      flash[:notice] = '新たな都市を追加しました。'
+      redirect_to admin_cities_path
+    else
+      @cities = City.all
+      render 'index'
+    end
+  end
+
+  def edit
+    @city = City.find(params[:id])
+  end
+
+  def update
+    @city = City.find(params[:id])
+    if @city.update(city_params)
+      flash[:notice] = '都市名を編集しました。'
+      redirect_to admin_cities_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @city = City.find(params[:id])
+    @city.destroy
+    flash[:notice] = '都市を削除しました。'
+    redirect_to admin_cities_path
+  end
+
+
+  
+  private
+  def city_params
+    params.require(:city).permit(:name)
+  end
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,2 +1,3 @@
 class Genre < ApplicationRecord
+    validates :name, presence: true, uniqueness: true
 end
