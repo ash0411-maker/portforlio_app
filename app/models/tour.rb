1a4732cffffc793a0ca26f08d392a34c83ea0527
ash <uetani.0411@gmail.com>
2020-06-19 15:13:16 +0900

    [add] Guide　ツアー新規投稿画面の作成

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -5,4 +5,5 @@ class Tour < ApplicationRecord
   belongs_to :genre, optional: true
   belongs_to :city, optional: true
   has_many :tour_photos, dependent: :destroy
+  accepts_nested_attributes_for :tour_photos, reject_if: :all_blank, allow_destroy: true
 end
