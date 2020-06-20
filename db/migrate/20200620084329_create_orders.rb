5643c702b05ccf7f0e53f681d5d50d165254614b
ash <uetani.0411@gmail.com>
2020-06-20 17:45:58 +0900

    [add] Orderモデルの作成

---
+++ db/migrate/20200620084329_create_orders.rb
@@ -0,0 +1,25 @@
+class CreateOrders < ActiveRecord::Migration[6.0]
+  def change
+    create_table :orders do |t|
+
+      t.timestamps
+      t.integer 'status', default: 0
+      t.integer 'tourist_id', null: false
+      t.integer 'tour_id', null: false
+      t.integer 'guide_id', null: false
+      t.string 'tour_title', null: false
+      t.text 'tour_body', null: false
+      t.integer 'tour_capacity', null: false
+      t.string 'tour_contents_of_price', null: false
+      t.string 'tour_time', null: false
+      t.string 'tour_city', null: false
+      t.string 'tour_genre', null: false
+      t.integer 'tour_price', null: false
+      t.datetime 'tour_date', null: false
+      t.integer 'total_people', null: false
+      t.integer 'total_price', null: false
+      t.integer 'admin_sales', null: false
+      t.integer 'guide_sales', null: false
+    end
+  end
+end
