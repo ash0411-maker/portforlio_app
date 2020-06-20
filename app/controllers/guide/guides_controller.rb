4863297261fe48999edf19868e800b7d9dc44b3a
ash <uetani.0411@gmail.com>
2020-06-21 08:08:12 +0900

    [add] Guide　マイページ閲覧機能　実装

--- app/controllers/guide/guides_controller.rb
+++ app/controllers/guide/guides_controller.rb
@@ -1,7 +1,22 @@
 class Guide::GuidesController < ApplicationController
+
+  before_action :authenticate_guide!, only: %i[show edit update destroy]
+  before_action :correct_guide, only: %i[show edit update destroy]
+
+
   def show
+    @guide = Guide.find(params[:id])
+    @tours = @guide.tours
   end
 
   def edit
+  end
+
+
+  private
+
+  def correct_guide
+    guide = Guide.find(params[:id])
+    redirect_to guide_guide_path(current_guide) if current_guide != guide
   end
 end
