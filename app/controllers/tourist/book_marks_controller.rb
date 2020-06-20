7410babaa2bd2740f719e790d818feeaa5c3f625
ash <uetani.0411@gmail.com>
2020-06-20 17:59:42 +0900

    [modified] Genre検索結果のgenre名の削除・book_markコントローラーのcorrectメソッド追加

--- app/controllers/tourist/book_marks_controller.rb
+++ app/controllers/tourist/book_marks_controller.rb
@@ -1,5 +1,6 @@
 class Tourist::BookMarksController < ApplicationController
   before_action :authenticate_tourist!
+  before_action :correct_tourist, only: %i[create destroy index]
 
   def index
     @book_marks = current_tourist.book_marks
@@ -15,5 +16,13 @@ class Tourist::BookMarksController < ApplicationController
     @tour = Tour.find(params[:tour_id])
     book_mark = current_tourist.book_marks.find_by(tour_id: @tour.id)
     book_mark.destroy
+  end
+
+
+  private
+
+  def correct_tourist
+    tourist = Tourist.find(params[:tourist_id])
+    redirect_to tourist_tourist_path(current_tourist) if current_tourist != tourist
   end
 end
