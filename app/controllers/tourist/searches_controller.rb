e6d8a5de980d6d64435255898b837a8f5f6a891f
ash <uetani.0411@gmail.com>
2020-06-20 14:32:22 +0900

    [add] 検索機能 検索結果の表示を実装

--- app/controllers/tourist/searches_controller.rb
+++ app/controllers/tourist/searches_controller.rb
@@ -1,2 +1,16 @@
 class Tourist::SearchesController < ApplicationController
+  before_action :authenticate_tourist!
+
+  def city
+    if @city = params['search']['city']
+      city_search_result = City.match(@city)
+      binding.pry
+      if city_search_result.empty?
+        @city_tours = empty
+      else
+        @city_tours = city.tours.page(params[:page]).per(9)
+        @genres = Genre.all
+      end
+    end
+  end
 end
--- app/models/city.rb
+++ app/models/city.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
 class City < ApplicationRecord
+  has_many :tours, dependent: :destroy
   validates :name, presence: true, uniqueness: true
+
+  def self.match(city_name)
+    City.find_by('(name LIKE ? )', "%#{city_name}%")
+  end
 end
