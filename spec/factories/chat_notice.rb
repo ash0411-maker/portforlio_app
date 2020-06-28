1f6b8f3ac45a20518c5e99f1b93c031ab4fcbe8d
ash <uetani.0411@gmail.com>
2020-06-28 14:07:46 +0900

    [add] Rspec model
    chat_noticeモデル実装

--- spec/factories/chat_notice.rb
+++ spec/factories/chat_notice.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
     factory :chat_notice do
         association :chat
-        association :guide_visitor
-        association :guide_visited
-        association :tourist_visitor
-        association :tourist_visited
+        association :guide_visitor, factory: :guide
+        association :guide_visited, factory: :guide
+        association :tourist_visitor, factory: :tourist
+        association :tourist_visited, factory: :tourist
         checked { Faker::Boolean.boolean }
     end
 end
