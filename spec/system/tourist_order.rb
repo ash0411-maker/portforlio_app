260265726dc25979b3b89e9444755d73d75c5225
ash <uetani.0411@gmail.com>
2020-06-29 18:42:35 +0900

    Rspec tourist_orderのテスト実装中
    fill_in問題

---
+++ spec/system/tourist_order.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe 'Tourist', type: :system do
+    let(:tourist) { create(:tourist) }
+    let(:another_tourist) { create(:another_tourist) }
+    let(:order) { create(:order) }
+
+
+
+    describe '予約テスト' do
+        context '予約人数入力' do
+            before do
+                login tourist
+                visit new_tourist_tourist_order_path(tourist)
+            end
+            it '予約確認画面へ遷移する' do
+                fill_in 'order_total_peole', with: '3'
+                click_on '予約内容確認へ'
+                expect(current_path).to eq(tourist_tourist_order_confirm_path(tourist))
+            end
+        end
+    end
+end
+
