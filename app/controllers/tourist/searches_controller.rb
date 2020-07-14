4c80e4fc58bf196675c0e7a70ddd47f252dbb878
ash <uetani.0411@gmail.com>
2020-07-14 16:38:29 +0900

    [modified] 日本語化・viewの修正

--- app/controllers/tourist/searches_controller.rb
+++ app/controllers/tourist/searches_controller.rb
@@ -4,13 +4,19 @@ class Tourist::SearchesController < ApplicationController
   def city
     if @city = params['search']['city']
       city_search_result = City.match(@city)
-      if city_search_result.nil?
+      if @city == ""
+        @city_tours = Tour.all.page(params[:page]).per(9)
+        @genres = Genre.all
+        flash[:notice] = "都市名を入力してください"
+      elsif city_search_result.nil?
         @city_tours = nil
         @genres = Genre.all
         @cities = City.all
+        flash[:notice] = ""
       else
         @city_tours = city_search_result.tours.page(params[:page]).per(9)
         @genres = Genre.all
+        flash[:notice] = ""
       end
     end
   end
