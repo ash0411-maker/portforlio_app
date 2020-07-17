b489eaea2fcb86ef3fb387dcc76a2c9db009307a
ash <uetani.0411@gmail.com>
2020-07-17 21:16:57 +0900

    [fix] 通知機能
    通知データが作成されていないバグを修正

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -4,6 +4,6 @@ class Notification < ApplicationRecord
   default_scope -> { order(created_at: :desc) }
   belongs_to :tour
   belongs_to :tourist
-  belongs_to :review
+  belongs_to :review, optional: true
   belongs_to :guide
 end
