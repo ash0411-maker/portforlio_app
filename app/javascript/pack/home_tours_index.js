45b9db8e78e0af1d85840141d88869b1e52455b7
ash <uetani.0411@gmail.com>
2020-07-20 10:11:00 +0900

    [modified] jQuery　修正
    jQueryのクラス名修正

--- app/javascript/pack/home_tours_index.js
+++ app/javascript/pack/home_tours_index.js
@@ -10,3 +10,5 @@ $(function(){
         });
     });
 });
+
+console.log("hello");
--- app/javascript/pack/order.js
+++ app/javascript/pack/order.js
@@ -1,7 +1,7 @@
 $(function() {
     $('.order-tab li').click(function() {
         var index = $('.order-tab li').index(this);
-        $('.order-tab li').removeClass('active');
+        $('.order-tab li').removeClass('order-active');
         $(this).addClass('order-active');
         // $('.area ul').removeClass('show').eq(index).addClass('show');
     });
