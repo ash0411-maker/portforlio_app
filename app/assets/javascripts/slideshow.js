3c147054af1d4394dd5b701dab71f084825df1ac
ash <uetani.0411@gmail.com>
2020-08-05 13:47:36 +0900

    slick表示の修正
    設定の変更

--- app/assets/javascripts/slideshow.js
+++ app/assets/javascripts/slideshow.js
@@ -1,10 +1,31 @@
 $(function() {
-    $('.slick').slick({
+    $('.slick-box').slick({
         dots: true,
+        centerMode: true,
+        centerPadding: '10%',
         autoplay: true,
-        fade: true,
-        autoplaySpeed: 3000,
-        adaptiveHeight: false
+        autoplaySpeed: 2000,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                arrows: false,
+                centerMode: false,
+                centerPadding: '0px',
+                slidesToShow: 1
+            }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    dots: false,
+                    autoplay: true,
+                    fade: true,
+                    centerMode: false,
+                    centerPadding: '0px'
+                }
+            }
+        ]
     });
 })
 
