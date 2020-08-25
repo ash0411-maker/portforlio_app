74cdba304ee703562696a3bded4fd35d01767a76
ash <uetani.0411@gmail.com>
2020-08-25 17:13:31 +0900

    [add]News トップ画面でアプリ内ニュース　作成時間を表示
    テキストの横に何時間前のデータかを表示

--- app/models/news.rb
+++ app/models/news.rb
@@ -1,4 +1,5 @@
 class News < ApplicationRecord
+    default_scope -> { order(created_at: :desc) }
     belongs_to :tour
     belongs_to :tourist, optional: true
     belongs_to :guide, optional: true
