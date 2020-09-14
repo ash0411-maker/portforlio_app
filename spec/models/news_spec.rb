a98dc50dd95dbd6682b545805cf8e0ec70b2ff1c
ash <uetani.0411@gmail.com>
2020-09-14 13:59:26 +0900

    [add] News modelテストの追加
    アソシエージョンテスト

--- spec/models/news_spec.rb
+++ spec/models/news_spec.rb
@@ -3,5 +3,21 @@
 require 'rails_helper'
 
 RSpec.describe News, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'newsアソシエーションのテスト' do
+    context 'guideモデルとの関係' do
+      it 'N:1となっている' do
+        expect(Notification.reflect_on_association(:guide).macro).to eq :belongs_to
+      end
+    end
+    context 'touristモデルとの関係' do
+      it 'N:1となっている' do
+        expect(Notification.reflect_on_association(:tourist).macro).to eq :belongs_to
+      end
+    end
+    context 'tourモデルとの関係' do
+      it 'N:1となっている' do
+        expect(Notification.reflect_on_association(:tour).macro).to eq :belongs_to
+      end
+    end
+  end
 end
