1263111cded24e2f1f0ff55899b74e31893af35c
ash <uetani.0411@gmail.com>
2020-10-01 14:17:29 +0900

    [delete] Newsコントローラーの削除

--- app/helpers/guide/news_helper.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-
-module Guide::NewsHelper
-end
--- spec/requests/guide/news_request_spec.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-require 'rails_helper'
-
-RSpec.describe 'Guide::News', type: :request do
-end
